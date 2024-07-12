c55e173f89ca23415f18e25925c03000ec73882b
Frank Mwito <Mfranklyne039@gmail.com>
2024-07-12 11:53:50 +0300

    Jetpack compose themes integration

--- app/src/main/java/com/example/jetpackcomposethemes/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposethemes/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -15,7 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposethemes.ui.theme.JetpackComposeThemesTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,10 +40,13 @@ fun themes()  {
     Scaffold(
         topBar = {
             TopAppBar(
+                modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    .background(MaterialTheme.colorScheme.onBackground),
                 title = {
                Text ("Jetpack Compose Themes",
                    style = MaterialTheme.typography.bodyLarge,
-                   color = MaterialTheme.colorScheme.onPrimary)
+                   textAlign = TextAlign.Center,
+                   color = MaterialTheme.colorScheme.onBackground)
                 },
                 actions = {
 
